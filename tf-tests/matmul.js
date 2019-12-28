1254d8edcc8fae57a3da204658269f998bc6d95b
Mathieu Poliquin <mathieu.poliquin@gmail.com>
2019-12-28 12:11:59 -0500

    Updated gflops test page

--- tf-tests/matmul.js
+++ tf-tests/matmul.js
@@ -1,81 +1,21 @@
-function Test() {
-    // Define a model for linear regression.
-    const model = tf.sequential();
-    model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-  
-    // Prepare the model for training: Specify the loss and the optimizer.
-    model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-  
-    // Generate some synthetic data for training.
-    const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-    const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
-  
-    // Train the model using the data.
-    model.fit(xs, ys).then(() => {
-    // Use the model to do inference on a data point the model hasn't seen before:
-    // Open the browser devtools to see the output
-    test = tf.memory().numBytesInGPU;
-  
-    result = model.predict(tf.tensor2d([5], [1, 1]));
-  
-    var node = document.getElementById('div-test');
-    
-    node.innerHTML += test;
-    });
-  }
-
-
-
 async function MatMulTest() {
-
     const matSize = 2 * 1024;
-    const numIterations = 1;
-  
-  
+
     const mat1 = tf.ones([matSize, matSize], tf.float32);
     const mat2 = tf.ones([matSize, matSize], tf.float32);
     WriteOutput(`Created two [${matSize},${matSize}] matrices filled with ones <br>`);
-    WriteOutput(`Running  tf.matMul(mat1, mat2) ${numIterations} times <br>`);
-  
 
-    const test = await tf.time(() => tf.matMul(mat1, mat2));
-  
-    //warmup run
     const matmulTime = await tf.time(() => tf.matMul(mat1, mat2));
     WriteOutput(`matmulTime ${matmulTime.kernelMs} ms <br>`);
-    //result.dispose();
     
-    //var results;
-    //var t0 = performance.now();
-    //for (var i=0; i < numIterations; i++) {  
-    //  result[i] = tf.matMul(mat1, mat2);      
-    //}
+    var time = matmulTime.kernelMs;
 
-    
-
-    //for (var i=0; i < numIterations; i++) {
-    //  result[i].dataSync();
-    //}
-
-    //var t1 = performance.now();
-
-    //var total = t1 - t0;
-    var total = matmulTime.kernelMs;
-
-    //for (var i=0; i < numIterations; i++) {
-    //  result[i].dispose();
-    //}
-    
-    mat1.dispose();
-    mat2.dispose();
-  
-    time = total / numIterations;
-  
     totalFlops = 2 * Math.pow(matSize,3);
     gflops = 1.0e-9 * totalFlops / time;
     WriteOutput(gflops + " GFlops <br>");
-    
-    
     WriteValue('tr-matmul', `${time.toFixed(3)} ms  <br> ${gflops.toFixed(3)} GFlops/s`);
-  }
+
+    mat1.dispose();
+    mat2.dispose();
+}
   
