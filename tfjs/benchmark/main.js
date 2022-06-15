d59c98b089e6848ee86186bf12eff02b681d1f81
Mathieu Poliquin <mathieu.poliquin@gmail.com>
2022-06-14 22:16:50 -0400

    Update main.js

--- tfjs/benchmark/main.js
+++ tfjs/benchmark/main.js
@@ -133,8 +133,8 @@ async function Init()
     let wasm_simd = await tf.env().getAsync('WASM_HAS_SIMD_SUPPORT');
     WriteValue('WASM_HAS_MULTITHREAD_SUPPORT', CreateDropDown("opt-wasm-multi", [true, false], wasm_multi));
     WriteValue('WASM_HAS_SIMD_SUPPORT', wasm_simd);
-    //WriteValue('WASM_THREADCOUNT', tf.wasm.getThreadCount());
-    WriteValue('WASM_THREADCOUNT', 16);
+    WriteValue('WASM_THREADCOUNT', tf.wasm.getThreadsCount());
+    //WriteValue('WASM_THREADCOUNT', 16);
   }
      
   let backend = tf.getBackend();
